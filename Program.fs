d845785b565d3f3d0776e16681df0ff011003d95
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-04-19 04:33:33 -0400

    first commit

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open System.IO
+open System.Drawing
+
+let arr = String.Join(", ", [|"aaa"; "bbb"; "ccc"|])
+let reduceDemo (arr : array<string>) =
+    arr
+    //|> Array.reduce(fun acc elem -> sprintf "%s, %s" acc elem)
+    |> Array.reduce(+)
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
+
